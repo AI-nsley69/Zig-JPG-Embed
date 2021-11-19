b0e31c687172da32c1d4ba4784c8423a12cd55fe
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2021-11-19 12:46:11 +0100

    Update build.zig

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("Zig JPG Embed", "src/main.zig");
+    const exe = b.addExecutable("jpg-embeder", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
